b7c360143634558ae90caae7143ceec8680218d0
Narendran <narendev0610@gmail.com>
2020-09-17 23:38:20 +0530

    Initial commit

---
+++ lib/data/data.dart
@@ -0,0 +1,297 @@
+import 'package:flutter/material.dart';
+import 'package:netflix_ui_flutter/data/constants/constant.dart';
+import 'package:netflix_ui_flutter/models/models.dart';
+
+final Movie springContent = Movie(
+  name: "Spring",
+  imageUrl: AssetConstants.springTitle,
+  videoUrl: AssetConstants.springVideoSource,
+  description:
+      'Spring is the story of a shepherd girl and her dog, who face ancient spirits in order to continue the cycle of life.',
+);
+
+final List<Movie> previews = const [
+  Movie(
+    name: 'Hubie Halloween',
+    imageUrl: AssetConstants.hubieHalloween,
+    color: Colors.yellow,
+    titleImageUrl: AssetConstants.hubieHalloweenTitle,
+  ),
+  Movie(
+    name: "Dark",
+    imageUrl: AssetConstants.dark,
+    color: Colors.green,
+    titleImageUrl: AssetConstants.darkTitle,
+  ),
+  Movie(
+    name: "The OA",
+    imageUrl: AssetConstants.theOA,
+    color: Colors.white,
+    titleImageUrl: AssetConstants.theOATitle,
+  ),
+  Movie(
+    name: "Biohackers",
+    imageUrl: AssetConstants.bioHackers,
+    color: Colors.blue,
+    titleImageUrl: AssetConstants.bioHackersTitle,
+  ),
+  Movie(
+    name: 'Lucifer',
+    imageUrl: AssetConstants.lucifer,
+    color: Colors.red,
+    titleImageUrl: AssetConstants.luciferTitle,
+  ),
+  Movie(
+    name: '13 Reasone Why',
+    imageUrl: AssetConstants.thirteenReasonsWhy,
+    color: Colors.deepOrange,
+    titleImageUrl: AssetConstants.thirteenReasonsWhyTitle,
+  ),
+  Movie(
+    name: 'Hubie Halloween',
+    imageUrl: AssetConstants.hubieHalloween,
+    color: Colors.yellow,
+    titleImageUrl: AssetConstants.hubieHalloweenTitle,
+  ),
+  Movie(
+    name: "Dark",
+    imageUrl: AssetConstants.dark,
+    color: Colors.green,
+    titleImageUrl: AssetConstants.darkTitle,
+  ),
+  Movie(
+    name: "The OA",
+    imageUrl: AssetConstants.theOA,
+    color: Colors.white,
+    titleImageUrl: AssetConstants.theOATitle,
+  ),
+  Movie(
+    name: "Biohackers",
+    imageUrl: AssetConstants.bioHackers,
+    color: Colors.blue,
+    titleImageUrl: AssetConstants.bioHackersTitle,
+  ),
+  Movie(
+    name: 'Lucifer',
+    imageUrl: AssetConstants.lucifer,
+    color: Colors.red,
+    titleImageUrl: AssetConstants.luciferTitle,
+  ),
+  Movie(
+    name: '13 Reasone Why',
+    imageUrl: AssetConstants.thirteenReasonsWhy,
+    color: Colors.deepOrange,
+    titleImageUrl: AssetConstants.thirteenReasonsWhyTitle,
+  ),
+];
+
+final List<Movie> myList = const [
+  Movie(
+    name: 'Anna',
+    imageUrl: AssetConstants.anna,
+  ),
+  Movie(
+    name: "Ava",
+    imageUrl: AssetConstants.ava,
+  ),
+  Movie(
+    name: 'Black Widow',
+    imageUrl: AssetConstants.blackWidow,
+  ),
+  Movie(
+    name: 'Endings Beginnings',
+    imageUrl: AssetConstants.endingsBeginnings,
+  ),
+  Movie(
+    name: 'Fast 9',
+    imageUrl: AssetConstants.fast9,
+  ),
+  Movie(
+    name: 'Honest Thief',
+    imageUrl: AssetConstants.honestTheif,
+  ),
+  Movie(
+    name: 'Irishman',
+    imageUrl: AssetConstants.irishMan,
+  ),
+  Movie(
+    name: 'Joker',
+    imageUrl: AssetConstants.joker,
+  ),
+  Movie(
+    name: 'Kajillionaire',
+    imageUrl: AssetConstants.kajillionaire,
+  ),
+  Movie(
+    name: 'The New Mutants',
+    imageUrl: AssetConstants.newMutants,
+  ),
+  Movie(
+    name: 'Anna',
+    imageUrl: AssetConstants.anna,
+  ),
+  Movie(
+    name: "Ava",
+    imageUrl: AssetConstants.ava,
+  ),
+  Movie(
+    name: 'Black Widow',
+    imageUrl: AssetConstants.blackWidow,
+  ),
+  Movie(
+    name: 'Endings Beginnings',
+    imageUrl: AssetConstants.endingsBeginnings,
+  ),
+  Movie(
+    name: 'Fast 9',
+    imageUrl: AssetConstants.fast9,
+  ),
+  Movie(
+    name: 'Honest Thief',
+    imageUrl: AssetConstants.honestTheif,
+  ),
+  Movie(
+    name: 'Irishman',
+    imageUrl: AssetConstants.irishMan,
+  ),
+  Movie(
+    name: 'Joker',
+    imageUrl: AssetConstants.joker,
+  ),
+  Movie(
+    name: 'Kajillionaire',
+    imageUrl: AssetConstants.kajillionaire,
+  ),
+  Movie(
+    name: 'The New Mutants',
+    imageUrl: AssetConstants.newMutants,
+  ),
+];
+
+final List<Movie> trending = const [
+  Movie(
+    name: 'Wonder Woman 84',
+    imageUrl: AssetConstants.wonderWomen84,
+  ),
+  Movie(
+    name: 'We summon Darkness',
+    imageUrl: AssetConstants.wesummondarkness,
+  ),
+  Movie(
+    name: 'Sputnik',
+    imageUrl: AssetConstants.sputnik,
+  ),
+  Movie(
+    name: 'Spiral',
+    imageUrl: AssetConstants.spiral,
+  ),
+  Movie(
+    name: 'Radioactive',
+    imageUrl: AssetConstants.radioactive,
+  ),
+  Movie(
+    name: 'Photograph',
+    imageUrl: AssetConstants.photograph,
+  ),
+  Movie(
+    name: 'Old Guard',
+    imageUrl: AssetConstants.oldGuard,
+  ),
+  Movie(
+    name: 'No Time To Die',
+    imageUrl: AssetConstants.noTimeToDie,
+  ),
+  Movie(
+    name: 'Night Clerk',
+    imageUrl: AssetConstants.nightClerk,
+  ),
+  Movie(
+    name: 'Wonder Woman 84',
+    imageUrl: AssetConstants.wonderWomen84,
+  ),
+  Movie(
+    name: 'We summon Darkness',
+    imageUrl: AssetConstants.wesummondarkness,
+  ),
+  Movie(
+    name: 'Sputnik',
+    imageUrl: AssetConstants.sputnik,
+  ),
+  Movie(
+    name: 'Spiral',
+    imageUrl: AssetConstants.spiral,
+  ),
+  Movie(
+    name: 'Radioactive',
+    imageUrl: AssetConstants.radioactive,
+  ),
+  Movie(
+    name: 'Photograph',
+    imageUrl: AssetConstants.photograph,
+  ),
+  Movie(
+    name: 'Old Guard',
+    imageUrl: AssetConstants.oldGuard,
+  ),
+  Movie(
+    name: 'No Time To Die',
+    imageUrl: AssetConstants.noTimeToDie,
+  ),
+  Movie(
+    name: 'Night Clerk',
+    imageUrl: AssetConstants.nightClerk,
+  ),
+];
+
+final List<Movie> originals = const [
+  Movie(
+    name: 'The Crown',
+    imageUrl:
+        'https://i.pinimg.com/originals/9c/9d/da/9c9dda37ef3df025545621c0fee81659.jpg',
+  ),
+  Movie(
+      name: 'Narcos',
+      imageUrl:
+          'https://i.pinimg.com/originals/16/65/bf/1665bf31830c8ff0d7af4174e29be849.jpg'),
+  Movie(
+      name: 'Stranger Things',
+      imageUrl:
+          'https://images-na.ssl-images-amazon.com/images/I/81QDU13hAAL._AC_SL1200_.jpg'),
+  Movie(
+      name: 'Sabrina',
+      imageUrl:
+          'https://i.pinimg.com/736x/49/ab/37/49ab376f7cdfca3735888bac94b5741c.jpg'),
+  Movie(
+      name: 'Ozark',
+      imageUrl:
+          'https://i.pinimg.com/originals/d4/fc/ea/d4fceadfcc7937aada4ee4925b8759aa.jpg'),
+  Movie(
+      name: 'House of Cards',
+      imageUrl:
+          'https://m.media-amazon.com/images/M/MV5BODM1MDU2NjY5NF5BMl5BanBnXkFtZTgwMDkxNTcwNjM@._V1_.jpg'),
+  Movie(
+    name: 'The Crown',
+    imageUrl:
+        'https://i.pinimg.com/originals/9c/9d/da/9c9dda37ef3df025545621c0fee81659.jpg',
+  ),
+  Movie(
+      name: 'Narcos',
+      imageUrl:
+          'https://i.pinimg.com/originals/16/65/bf/1665bf31830c8ff0d7af4174e29be849.jpg'),
+  Movie(
+      name: 'Stranger Things',
+      imageUrl:
+          'https://images-na.ssl-images-amazon.com/images/I/81QDU13hAAL._AC_SL1200_.jpg'),
+  Movie(
+      name: 'Sabrina',
+      imageUrl:
+          'https://i.pinimg.com/736x/49/ab/37/49ab376f7cdfca3735888bac94b5741c.jpg'),
+  Movie(
+      name: 'Ozark',
+      imageUrl:
+          'https://i.pinimg.com/originals/d4/fc/ea/d4fceadfcc7937aada4ee4925b8759aa.jpg'),
+  Movie(
+      name: 'House of Cards',
+      imageUrl:
+          'https://m.media-amazon.com/images/M/MV5BODM1MDU2NjY5NF5BMl5BanBnXkFtZTgwMDkxNTcwNjM@._V1_.jpg'),
+];
